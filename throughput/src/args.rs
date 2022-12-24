f33750619cb57948937d6b6844c0f7459d3df8a6
drblah <theblahblah2001@gmail.com>
2022-12-24 15:11:27 +0100

    throughput: Expand CLI to support UDP and QUIC and split Server/Client options

--- throughput/src/args.rs
+++ throughput/src/args.rs
@@ -1,14 +1,18 @@
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
 use std::net::IpAddr;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
 pub struct Opts {
     #[command(subcommand)]
-    mode: Modes,
+    mode: Option<Modes>,
 
     #[arg(long, action = clap::ArgAction::SetTrue)]
     dont_fragment: Option<bool>,
+
+    /// Provide a config file instead of the options above
+    #[arg(long)]
+    config_file: Option<String>,
 }
 
 #[derive(Subcommand, Debug)]
@@ -16,40 +20,104 @@ enum Modes {
     /// Set Niceperf throughput to run in server mode
     #[command(arg_required_else_help = true)]
     Server {
-        /// IP the server will listen on
-        #[arg(short, long, conflicts_with = "config_file")]
-        server_ip: Option<IpAddr>,
-
-        /// Port the server will listen on
-        #[arg(short, long, conflicts_with = "config_file")]
-        port: Option<u16>,
-
-        /// Interface to bind to
-        #[arg(short, long, conflicts_with = "config_file")]
-        interface: Option<String>,
-
-        /// Provide a config file instead of the options above
-        #[arg(long)]
-        config_file: Option<String>,
+        #[command(subcommand)]
+        proto: ServerProtocol,
     },
 
     /// Set Niceperf throughput to run in client mode
     #[command(arg_required_else_help = true)]
     Client {
-        /// IP the client will listen on
-        #[arg(short, long)]
-        client_ip: Option<IpAddr>,
-
-        /// Port the client will listen on
-        #[arg(short, long)]
-        port: Option<u16>,
-
-        /// Interface to bind to
-        #[arg(short, long)]
-        interface: Option<String>,
-
-        /// Provide a config file instead of the options above
-        #[arg(long)]
-        config_file: Option<String>,
+        #[command(subcommand)]
+        proto: ClientProtocol,
     },
+}
+
+#[derive(Subcommand, Debug, Clone)]
+enum ServerProtocol {
+    #[command(arg_required_else_help = true)]
+    Tcp(TcpServerOpts),
+
+    #[command(arg_required_else_help = true)]
+    Udp(UdpServerOpts),
+
+    #[command(arg_required_else_help = true)]
+    Quic(QuicServerOpts),
+}
+
+#[derive(Args, Clone, Debug)]
+struct TcpServerOpts {
+    #[command(flatten)]
+    server_common_opts: ServerCommonOpts,
+}
+
+#[derive(Args, Clone, Debug)]
+struct UdpServerOpts {
+    #[command(flatten)]
+    server_common_opts: ServerCommonOpts,
+}
+
+#[derive(Args, Clone, Debug)]
+struct QuicServerOpts {
+    #[command(flatten)]
+    server_common_opts: ServerCommonOpts,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+enum ClientProtocol {
+    #[command(arg_required_else_help = true)]
+    Tcp(TcpClientOpts),
+
+    #[command(arg_required_else_help = true)]
+    Udp(UdpClientOpts),
+
+    #[command(arg_required_else_help = true)]
+    Quic(QuicClientOpts),
+}
+
+#[derive(Args, Clone, Debug)]
+struct TcpClientOpts {
+    #[command(flatten)]
+    client_common_opts: ClientCommonOpts,
+}
+
+#[derive(Args, Clone, Debug)]
+struct UdpClientOpts {
+    #[command(flatten)]
+    client_common_opts: ClientCommonOpts,
+}
+
+#[derive(Args, Clone, Debug)]
+struct QuicClientOpts {
+    #[command(flatten)]
+    client_common_opts: ClientCommonOpts,
+}
+
+#[derive(Args, Clone, Debug)]
+struct ServerCommonOpts {
+    /// IP the server will listen on
+    #[arg(short, long)]
+    server_ip: Option<IpAddr>,
+
+    /// Port the server will listen on
+    #[arg(short, long)]
+    port: Option<u16>,
+
+    /// Interface to bind to
+    #[arg(short, long)]
+    interface: Option<String>,
+}
+
+#[derive(Args, Clone, Debug)]
+struct ClientCommonOpts {
+    /// IP of the server
+    #[arg(short = 'c', long = "client")]
+    server_ip: Option<IpAddr>,
+
+    /// Port of the server
+    #[arg(short, long)]
+    port: Option<u16>,
+
+    /// Interface to bind to
+    #[arg(short, long)]
+    interface: Option<String>,
 }
