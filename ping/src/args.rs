3bee8952274b177efc9d30a1cc0ecaca67ecb28c
suhrm <suhrminator@gmail.com>
2022-12-24 15:06:14 +0100

    Added More stuff

--- ping/src/args.rs
+++ ping/src/args.rs
@@ -30,10 +30,14 @@ enum Modes {
 #[derive(Subcommand, Debug, Clone)]
 enum Protocol {
     #[command(arg_required_else_help = true)]
-    Tcp(CommonOpts),
-
-    Udp(CommonOpts),
-    Icmp(CommonOpts),
+    /// Set Niceperf ping to run in TCP mode
+    Tcp(TCPOpts),
+    #[command(arg_required_else_help = true)]
+    /// Set Niceperf ping to run in UDP mode
+    Udp(UDPOpts),
+    #[command(arg_required_else_help = true)]
+    /// Set Niceperf ping to run in ICMP mode
+    Icmp(ICMPOpts),
 }
 
 #[derive(Args, Clone, Debug)]
@@ -53,4 +57,47 @@ struct CommonOpts {
     /// Duration of the test in seconds (mutually exclusive with count)
     #[arg(long, short, conflicts_with = "count")]
     duration: Option<u64>,
+}
+#[derive(Args, Clone, Debug)]
+struct TCPOpts {
+    #[command(flatten)]
+    common_opts: CommonOpts,
+    /// Set the source port
+    #[arg(long)]
+    src_port: Option<u16>,
+    /// Set the destination port
+    #[arg(long)]
+    dst_port: Option<u16>,
+    /// Set the destination address
+    #[arg(long)]
+    dst_addr: Option<IpAddr>,
+    /// Congetion control algorithm to use
+    #[arg(long, default_value = "reno")]
+    cc: Option<String>,
+    /// Maximum segment size
+    #[arg(long, short, default_value = "1460")]
+    mss: Option<u16>,
+}
+
+#[derive(Args, Clone, Debug)]
+struct UDPOpts {
+    #[command(flatten)]
+    common_opts: CommonOpts,
+    /// Set the source port
+    #[arg(long, short)]
+    src_port: Option<u16>,
+    /// Set the destination port
+    #[arg(long, short)]
+    dst_port: Option<u16>,
+    /// Set the destination address
+    #[arg(long, short)]
+    dst_addr: Option<IpAddr>,
+}
+#[derive(Args, Clone, Debug)]
+struct ICMPOpts {
+    #[command(flatten)]
+    common_opts: CommonOpts,
+    /// Set the destination address
+    #[arg(long, short)]
+    dst_addr: Option<IpAddr>,
 }
