0f57bb06053ef90c63b30a00e51ed5f66936109a
drblah <theblahblah2001@gmail.com>
2022-12-24 14:39:01 +0100

    throughput: Move CLI to Args file

---
+++ throughput/src/args.rs
@@ -0,0 +1,55 @@
+use clap::{Parser, Subcommand};
+use std::net::IpAddr;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+pub struct Opts {
+    #[command(subcommand)]
+    mode: Modes,
+
+    #[arg(long, action = clap::ArgAction::SetTrue)]
+    dont_fragment: Option<bool>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Modes {
+    /// Set Niceperf throughput to run in server mode
+    #[command(arg_required_else_help = true)]
+    Server {
+        /// IP the server will listen on
+        #[arg(short, long, conflicts_with = "config_file")]
+        server_ip: Option<IpAddr>,
+
+        /// Port the server will listen on
+        #[arg(short, long, conflicts_with = "config_file")]
+        port: Option<u16>,
+
+        /// Interface to bind to
+        #[arg(short, long, conflicts_with = "config_file")]
+        interface: Option<String>,
+
+        /// Provide a config file instead of the options above
+        #[arg(long)]
+        config_file: Option<String>,
+    },
+
+    /// Set Niceperf throughput to run in client mode
+    #[command(arg_required_else_help = true)]
+    Client {
+        /// IP the client will listen on
+        #[arg(short, long)]
+        client_ip: Option<IpAddr>,
+
+        /// Port the client will listen on
+        #[arg(short, long)]
+        port: Option<u16>,
+
+        /// Interface to bind to
+        #[arg(short, long)]
+        interface: Option<String>,
+
+        /// Provide a config file instead of the options above
+        #[arg(long)]
+        config_file: Option<String>,
+    },
+}
--- throughput/src/main.rs
+++ throughput/src/main.rs
@@ -1,61 +1,9 @@
-use clap::{Parser, Subcommand};
-use std::net::IpAddr;
+use clap::Parser;
 
-#[derive(Parser, Debug)]
-#[command(author, version, about)]
-struct Args {
-    #[command(subcommand)]
-    mode: Modes,
-
-    #[arg(long, action = clap::ArgAction::SetTrue)]
-    dont_fragment: Option<bool>,
-}
-
-#[derive(Subcommand, Debug)]
-enum Modes {
-    /// Set Niceperf throughput to run in server mode
-    #[command(arg_required_else_help = true)]
-    Server {
-        /// IP the server will listen on
-        #[arg(short, long, conflicts_with = "config_file")]
-        server_ip: Option<IpAddr>,
-
-        /// Port the server will listen on
-        #[arg(short, long, conflicts_with = "config_file")]
-        port: Option<u16>,
-
-        /// Interface to bind to
-        #[arg(short, long, conflicts_with = "config_file")]
-        interface: Option<String>,
-
-        /// Provide a config file instead of the options above
-        #[arg(long)]
-        config_file: Option<String>,
-    },
-
-    /// Set Niceperf throughput to run in client mode
-    #[command(arg_required_else_help = true)]
-    Client {
-        /// IP the client will listen on
-        #[arg(short, long)]
-        client_ip: Option<IpAddr>,
-
-        /// Port the client will listen on
-        #[arg(short, long)]
-        port: Option<u16>,
-
-        /// Interface to bind to
-        #[arg(short, long)]
-        interface: Option<String>,
-
-        /// Provide a config file instead of the options above
-        #[arg(long)]
-        config_file: Option<String>,
-    },
-}
+mod args;
 
 fn main() {
-    let args = Args::parse();
+    let args = args::Opts::parse();
 
     println!("{:?}", args)
 }
