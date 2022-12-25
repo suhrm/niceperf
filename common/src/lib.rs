49fa9ee5282dff4a633be38be29555ec6b5b1f0c
drblah <theblahblah2001@gmail.com>
2022-12-25 15:27:25 +0100

    Common: Add new_tcp_socket

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,7 +1,6 @@
 use anyhow::Result;
 use socket2::{Domain, Protocol, Socket, Type};
-use std::net::IpAddr;
-
+use std::net::{IpAddr, SocketAddrV4, SocketAddrV6};
 
 // Create new ICMP socket, default to IPv4
 pub fn new_icmp_socket(
@@ -21,7 +20,7 @@ pub fn new_icmp_socket(
 
                 match bind_interface {
                     Some(bi) => bind_to_device(socket, bi)?,
-                    None => socket
+                    None => socket,
                 }
             }
 
@@ -35,7 +34,7 @@ pub fn new_icmp_socket(
 
                 match bind_interface {
                     Some(bi) => bind_to_device(socket, bi)?,
-                    None => socket
+                    None => socket,
                 }
             }
         },
@@ -46,7 +45,7 @@ pub fn new_icmp_socket(
 
             match bind_interface {
                 Some(bi) => bind_to_device(socket, bi)?,
-                None => socket
+                None => socket,
             }
         }
     };
@@ -54,6 +53,40 @@ pub fn new_icmp_socket(
     Ok(socket)
 }
 
+pub fn new_tcp_socket(
+    bind_interface: Option<&str>,
+    bind_address: IpAddr,
+    bind_port: u16,
+) -> Result<Socket> {
+    let socket = match bind_address {
+        IpAddr::V4(address) => {
+            let socket = Socket::new(Domain::IPV4, Type::STREAM, None)?;
+
+            let socket_address = SocketAddrV4::new(address, bind_port);
+            socket.bind(&socket_address.into())?;
+            socket.set_nonblocking(true)?;
+
+            socket
+        }
+        IpAddr::V6(address) => {
+            let socket = Socket::new(Domain::IPV4, Type::STREAM, None)?;
+
+            // TODO: Figure out what is the correct way to use flowinfo and scope_id of IPv6 Sockets.
+            let socket_address = SocketAddrV6::new(address, bind_port, 0, 0);
+            socket.bind(&socket_address.into())?;
+            socket.set_nonblocking(true)?;
+
+            socket
+        }
+    };
+
+    let socket = match bind_interface {
+        Some(bi) => bind_to_device(socket, bi)?,
+        None => socket,
+    };
+
+    Ok(socket)
+}
 
 pub fn bind_to_device(
     socket: Socket,
@@ -63,12 +96,15 @@ pub fn bind_to_device(
     // In case, we get an error when binding, map it into a more friendly std::io::Error
     if let Err(err) = socket.bind_device(Some(bind_interface.as_bytes())) {
         return if matches!(err.raw_os_error(), Some(libc::ENODEV)) {
-            let error_msg = format!("error binding to device (`{}`): {}", bind_interface, err);
+            let error_msg = format!(
+                "error binding to device (`{}`): {}",
+                bind_interface, err
+            );
             Err(std::io::Error::new(std::io::ErrorKind::Other, error_msg))
         } else {
             let error_msg = format!("unexpected error binding device: {}", err);
             Err(std::io::Error::new(std::io::ErrorKind::Other, error_msg))
-        }
+        };
     }
 
     Ok(socket)
