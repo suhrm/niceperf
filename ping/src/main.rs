0ada8d935756143c8cbcfb7fff76e4104a011784
suhrm <suhrminator@gmail.com>
2022-12-26 22:37:22 +0100

    Work in progress.

--- ping/src/main.rs
+++ ping/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
 
 struct ICMPClient {
     /// ICMP socket
-    socket: ICMPSocket,
+    socket: AsyncICMPSocket,
     /// Interface to bind to
     iface: String,
     /// Src IP address of the socket
@@ -36,8 +36,9 @@ impl ICMPClient {
         let src_addr = interface_to_ipaddr(iface)?;
         let dst_addr = args.dst_addr;
         let socket = ICMPSocket::new(Some(iface), None)?;
+
         Ok(ICMPClient {
-            socket,
+            socket: AsyncICMPSocket::new(socket)?,
             iface: iface.to_string(),
             src_addr,
             dst_addr,
@@ -47,37 +48,77 @@ impl ICMPClient {
         })
     }
     pub async fn run(&mut self) -> Result<()> {
-        Ok(())
+        let mut buf = [0u8; 1024];
+
+        let mut pacing_timer =
+            tokio::time::interval(std::time::Duration::from_millis(1000));
+
+        loop {
+            tokio::select! {
+                _ = pacing_timer.tick() => {
+                    self.socket.send_to(&buf, &self.dst_addr).await?;
+
+                },
+                _ = self.socket.read(&mut buf) => {
+
+
+                }
+            }
+        }
     }
 }
 
-struct AsyncICMP {
+struct AsyncICMPSocket {
     inner: AsyncFd<ICMPSocket>,
 }
 
-impl AsyncICMP {
-    pub fn new(socket: ICMPSocket) -> Result<AsyncICMP> {
-        Ok(AsyncICMP {
+impl AsyncICMPSocket {
+    pub fn new(socket: ICMPSocket) -> Result<Self> {
+        Ok(Self {
             inner: AsyncFd::new(socket)?,
         })
     }
 
-    /* pub async fn send(&mut self, packet: &[u8]) -> Result<()> {
-        let mut guard = self.inner.writable().await;
-        guard.try_io(|inner| inner.get_mut().send(packet))?;
-        Ok(())
-    } */
+    pub async fn send_to(
+        &mut self,
+        packet: &[u8],
+        addr: &IpAddr,
+    ) -> Result<(usize)> {
+        let mut guard = self.inner.writable().await?;
+        let addr = match addr {
+            IpAddr::V4(addr) => {
+                let addr = std::net::SocketAddr::V4(
+                    std::net::SocketAddrV4::new(*addr, 0),
+                );
+                socket2::SockAddr::from(addr)
+            }
+            IpAddr::V6(addr) => {
+                // TODO : Check if this is correct
+                let addr = std::net::SocketAddr::V6(
+                    std::net::SocketAddrV6::new(*addr, 0, 0, 0),
+                );
+                socket2::SockAddr::from(addr)
+            }
+        };
+        match guard
+            .try_io(|inner| inner.get_ref().get_ref().send_to(packet, &addr))
+        {
+            Ok(res) => Ok(res?),
+            Err(e) => Err(anyhow!("Error sending packet")),
+        }
+    }
 
-    pub async fn recv(&mut self, buf: &mut [u8]) -> Result<usize> {
+    pub async fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
         loop {
             let mut guard = self.inner.readable().await?;
+            // Safety: We are sure that the buffer is initialized
             let uninit_slice = unsafe { core::mem::transmute(&mut *buf) };
 
             match guard
                 .try_io(|inner| inner.get_ref().get_ref().recv(uninit_slice))
             {
                 Ok(Ok(n)) => return Ok(n),
-                Ok(Err(e)) => return Err(e.into()),
+                Ok(Err(e)) => Err(anyhow!(e.to_string()))?,
                 Err(_would_block) => continue,
             }
         }
