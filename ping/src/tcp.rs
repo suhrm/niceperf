016babf9dfd1df07bf99077fd97f5a351169c333
suhrm <suhrminator@gmail.com>
2023-10-22 11:13:10 +0200

    wip

--- ping/src/tcp.rs
+++ ping/src/tcp.rs
@@ -282,9 +282,8 @@ impl TCPServer {
         loop {
             tokio::select! {
                 Ok(len) = rx.read_u16() => {
-                    panic!("len: {}", len);
                     rx.read_exact(&mut buffer[..len as usize]).await?;
-                    println!("buffer: {:?}", buffer);
+                    tx.write_u16(len).await?;
                     tx.write_all(&buffer).await?;
                 },
                 else => {
